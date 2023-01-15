e180ced1911ac84593b0b14a0a70f6dfeff1635f
k5924 <k.bhatti5384@yahoo.co.uk>
2023-01-15 17:28:37 +0000

    Subject: HTTP is the Platform Task: Enable basic requests Solution: Change package name to be correct

--- src/main/java/com/example/Payroll/Employee.java
+++ src/main/java/com/example/Payroll/Employee.java
@@ -1,10 +1,10 @@
-package payroll;
+package com.example.Payroll;
 
 import java.util.Objects;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
 @Entity
 class Employee {
--- src/main/java/com/example/Payroll/EmployeeRepository.java
+++ src/main/java/com/example/Payroll/EmployeeRepository.java
@@ -1,4 +1,4 @@
-package payroll;
+package com.example.Payroll;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
